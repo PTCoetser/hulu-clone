9fe14064b1bfa9feade2910c521e7f57d922d18e
Paul Coetser <ptcoetser@gmail.com>
2023-08-18 15:44:29 +0100

    Updated Deployment Script

--- src/App.js
+++ src/App.js
@@ -17,55 +17,55 @@ import SeasonDetail from "./Components/SeasonDetail";
 import Similars from "./Components/Similars";
 
 function App() {
-	const [genre, setGenre] = useState(requests.fetchAction);
-	const [watchList, setWatchList] = useState(existingWatchlist);
+  const [genre, setGenre] = useState(requests.fetchAction);
+  const [watchList, setWatchList] = useState(existingWatchlist);
 
-	return (
-		<div className="app">
-			<Router>
-				<Route path="/:mediaType/:movieID" exact>
-					<Detail watchList={watchList} setWatchList={setWatchList} />
-				</Route>
-				<Route path="/:mediaType/:movieID/recommendations" exact>
-					<Similars />
-				</Route>
-				<Route path="/" exact>
-					<Header />
-					<Nav setGenre={setGenre} />
-					<Results genre={genre} />
-				</Route>
-				<Route path="/trending" exact>
-					<Header />
-					{/* <Nav setGenre={setGenre} /> */}
-					<Results genre={requests.fetchTrending} />
-				</Route>
-				<Route path="/watchlist" exact>
-					<Header />
-					<WatchList watchList={watchList} setWatchList={setWatchList} />
-				</Route>
-				<Route path="/search">
-					<Header />
-					<Search />
-				</Route>
-				<Route path="/searchPeople" exact>
-					<Header />
-					<SearchPeople />
-				</Route>
-				<Route path="/searchPeople/:id/knownfor">
-					<KnownForMovies />
-				</Route>
-				<Route path="/:mediaType/:movieID/cast" exact>
-					<Cast />
-				</Route>
-				<Route path="/:mediaType/:movieID/seasons" exact>
-					<Seasons />
-				</Route>
-				<Route path="/:mediaType/:movieID/seasons/season/:seasonNumber" exact>
-					<SeasonDetail />
-				</Route>
-			</Router>
-		</div>
-	);
+  return (
+    <div className="app">
+      <Router>
+        <Route path="/:mediaType/:movieID" exact>
+          <Detail watchList={watchList} setWatchList={setWatchList} />
+        </Route>
+        <Route path="/:mediaType/:movieID/recommendations" exact>
+          <Similars />
+        </Route>
+        <Route path="/" exact>
+          <Header />
+          <Nav setGenre={setGenre} />
+          <Results genre={genre} />
+        </Route>
+        <Route path="/trending" exact>
+          <Header />
+          {/* <Nav setGenre={setGenre} /> */}
+          <Results genre={requests.fetchTrending} />
+        </Route>
+        <Route path="/watchlist" exact>
+          <Header />
+          <WatchList watchList={watchList} setWatchList={setWatchList} />
+        </Route>
+        <Route path="/search">
+          <Header />
+          <Search />
+        </Route>
+        <Route path="/searchPeople" exact>
+          <Header />
+          <SearchPeople />
+        </Route>
+        <Route path="/searchPeople/:id/knownfor">
+          <KnownForMovies />
+        </Route>
+        <Route path="/:mediaType/:movieID/cast" exact>
+          <Cast />
+        </Route>
+        <Route path="/:mediaType/:movieID/seasons" exact>
+          <Seasons />
+        </Route>
+        <Route path="/:mediaType/:movieID/seasons/season/:seasonNumber" exact>
+          <SeasonDetail />
+        </Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
